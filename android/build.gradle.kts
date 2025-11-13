37d186188ce3eae36ec1719bb70b43f519bf69c8
bzunderdog <brandonzabawa123@gmail.com>
2025-11-13 01:29:02 -0500

    Update on app design - Senior Design Parcel 1. Changed the repository being dev on to new one to fix gradle issues 2. Changing the layout of the app overall. 3. Will be updating dashboards appearance per day. 4. Will do final touches this Saturday to make code more user-intuitive

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,27 +4,6 @@ allprojects {
         mavenCentral()
     }
 }
-plugins {
-    // versions already declared in settings.gradle.kts
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    // do NOT add dev.flutter.flutter-gradle-plugin here
-}
-//plugins {
-//    id("com.android.application") version "8.6.1" apply false
-//    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-//    id("com.google.gms.google-services") version "4.4.2" apply false
-//    // Do NOT declare a version for dev.flutter.flutter-gradle-plugin here;
-//    // it’s supplied via includeBuild(...) in settings.gradle.kts.
-//}
-
-//plugins {
-//    id("com.android.application") apply false
-//    id("com.android.library")     apply false
-//    id("org.jetbrains.kotlin.android") apply false
-//    // You can keep a version here if you want, it rarely collides:
-//    id("com.google.gms.google-services") apply false
-//}
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
@@ -39,9 +18,7 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-tasks.register("clean", Delete::class){
-    delete(rootProject.buildDir)
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
-//tasks.register<Delete>("clean") {
-//    delete(rootProject.layout.buildDirectory)
-//}
